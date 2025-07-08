b7d8f0c30025b8302cc6a8ffd729ebf62c944af3
Pravin <pravin@123gmail.com>
2025-07-08 15:13:32 +0530

    New Implementation in ProductRestController

--- 05-Product_Service/src/main/java/com/thetiebar/contoller/ProductRestController.java
+++ 05-Product_Service/src/main/java/com/thetiebar/contoller/ProductRestController.java
@@ -6,7 +6,9 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -73,7 +75,7 @@ public class ProductRestController {
 	
 	@GetMapping
 	public ResponseEntity<ApiResponse<List<ProductDto>>> getAllProducts(){
-		Map<String, String> messages = props.getMessages();
+//		Map<String, String> messages = props.getMessages();
 		
 		List<ProductDto> products = productService.getAllProducts();
 		ApiResponse<List<ProductDto>> response = new ApiResponse<>();
@@ -88,6 +90,58 @@ public class ProductRestController {
 		}
 		
 		return ResponseEntity.status(HttpStatus.OK).body(response);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<ApiResponse<ProductDto>> getProductById(@PathVariable("id")Integer productId){
+//		Map<String, String> messages = props.getMessages();
+		
+		ProductDto product = productService.getProductById(productId);
+		ApiResponse<ProductDto> response = new ApiResponse<>();
+		
+		if(product != null) {
+			response.setStatusCode(200);
+			response.setMessage("Product Got ");
+			response.setData(product);
+		}else {
+			response.setMessage("Product not Found");
+			response.setStatusCode(500);
+		}
+		
+		return ResponseEntity.status(HttpStatus.OK).body(response);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<ApiResponse<ProductDto>> deleteProductById(@PathVariable("id") Integer productId){
+		
+//		Map<String, String> messages = props.getMessages();
+		
+		ProductDto updatedProduct = productService.deleteProductById(productId);
+		ApiResponse<ProductDto> response = new ApiResponse<>();
+		
+		if(updatedProduct != null) {
+			response.setData(updatedProduct);
+			response.setMessage("Product Updated Successfully");
+			response.setStatusCode(201);
+		}else {
+			response.setStatusCode(500);
+			response.setMessage("Product is Not Available");
+		}
+		
+		return ResponseEntity.status(HttpStatus.OK).body(response);
+	}
+	
+	@PatchMapping("/{id}/stock")
+	public ResponseEntity<String> updateStock(@PathVariable("id")Integer productId, @RequestParam("quantity") Integer quantity){
+		
+		boolean update = productService.updateStock(productId, quantity);
+		
+		if(update) {
+			return ResponseEntity.ok("Stock Updated Succefully");
+		} else {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Failed to update stock");
+		}
+		
 	}
 	
 }
